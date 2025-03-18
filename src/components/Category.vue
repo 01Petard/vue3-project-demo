df9cc756be176584ce1686e0562f0bde23f80ad6
01Petard <1520394133@qq.com>
2025-03-18 21:48:50 +0800

    slot

---
+++ src/components/Category.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps(['title'])
+</script>
+
+<template>
+  <div class="category">
+    <h2>{{ title }}</h2>
+    <slot>默认内容</slot>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  background-color: firebrick;
+  text-align: center;
+}
+
+.category {
+  background-color: cadetblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
 
+import Category from "@/components/Category.vue";
+import {reactive} from "vue";
+
+let newsList = reactive([
+  {id: 'asdftrfay01', title: '很好的抗癌食物', content: '西兰花'},
+  {id: 'asdftrfay02', title: '如何一夜暴富', content: '学IT'},
+  {id: 'asdftrfay03', title: '震惊，万万没想到', content: '明天是周一'}
+]);
+
+let imgsrc = 'https://img.yzcdn.cn/vant/cat.jpeg';
+
+let videosrc = 'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4';
+
 </script>
 
 <template>
@@ -7,8 +20,37 @@
     <h2>关于</h2>
     <p>这是关于内容。</p>
   </div>
+
+  <div class="content">
+
+    <Category title="文字">
+      <ul>
+        <li v-for="news in newsList" :key="news.id">{{ news.title }}</li>
+      </ul>
+    </Category>
+
+    <Category title="图片">
+      <img :src="imgsrc"/>
+
+    </Category>
+
+    <Category title="视频">
+      <video :src="videosrc" controls/>
+
+    </Category>
+  </div>
+
+
 </template>
 
 <style scoped>
+.content {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+img, video {
+  width: 200px;
+}
 
 </style>
